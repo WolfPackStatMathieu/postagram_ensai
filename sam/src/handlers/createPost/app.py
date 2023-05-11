221d1a569dcb7c0778b9dfcf460e423da5f0a2f1
odi77 <cnebanga@gmail.com>
2023-05-11 11:59:20 +0200

    post

---
+++ sam/src/handlers/createPost/app.py
@@ -0,0 +1,59 @@
+import json
+import uuid
+import boto3
+import os
+
+# import requests
+
+
+def lambda_handler(event, context):
+    """Sample pure Lambda function
+
+    Parameters
+    ----------
+    event: dict, required
+        API Gateway Lambda Proxy Input Format
+
+        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+
+    context: object, required
+        Lambda Context runtime methods and attributes
+
+        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
+
+    Returns
+    ------
+    API Gateway Lambda Proxy Output Format: dict
+
+        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+    """
+
+
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(os.getenv("POSTS_TABLE"))
+    
+    body = json.loads(event['body'])
+    # get username
+    user = event["headers"]["Authorization"]
+
+    
+    # object to be inserted
+    data = table.put_item(
+            Item = { 
+                    'id': str(uuid.uuid4()),
+                    'user': body['user'],
+                    'title': body['title'],
+                    'body': body['body']
+                    }
+                )
+
+    response = {
+                "statusCode": 200,
+                "headers": {
+                'Access-Control-Allow-Origin': '*'
+            },
+            "body":json.dumps(data)
+        }
+    
+    return response
+
